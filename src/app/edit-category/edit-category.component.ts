b34eee38aaca87811e3be878f7f6b3932bfefc48
felipemendes <f.ribeiromendes@gmail.com>
2018-10-14 03:31:39 -0300

    Implement get categories

--- src/app/edit-category/edit-category.component.ts
+++ src/app/edit-category/edit-category.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from "../service/category.service";
+import { Router } from "@angular/router";
+import { Category } from "../model/category.model";
+import { FormBuilder, FormGroup, Validators}  from "@angular/forms";
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-category',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCategoryComponent implements OnInit {
 
-  constructor() { }
+  category: Category;
+  editForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,private router: Router, private categoryService: CategoryService) { }
 
   ngOnInit() {
+    let categoryUuid = localStorage.getItem("editCategoryUuid");
+    if(!categoryUuid) {
+      alert("Invalid action.")
+      this.router.navigate(['list-category']);
+      return;
+    }
+
+    this.editForm = this.formBuilder.group({
+      id: [],
+      uuid: [],
+      status: ['', Validators.required],
+      title: ['', Validators.required],
+      url_image: ['', Validators.required]
+    });
+
+    this.categoryService.getCategoryByUuid(categoryUuid)
+      .subscribe( data => {
+        this.editForm.setValue(data.categories[0]);
+      });
+  }
+
+  onSubmit() {
+    this.categoryService.updateCategory(this.editForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['list-category']);
+        },
+        error => {
+          alert(error);
+        }
+      );
   }
 
 }
