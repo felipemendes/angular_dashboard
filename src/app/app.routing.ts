7fde4966d0479766c027265a966d4643223e1a89
felipemendes <f.ribeiromendes@gmail.com>
2018-10-13 22:15:11 -0300

    Add routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AddCategoryComponent} from "./add-category/add-category.component";
+import {ListCategoryComponent} from "./list-category/list-category.component";
+import {EditCategoryComponent} from "./edit-category/edit-category.component";
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'add-category', component: AddCategoryComponent },
+  { path: 'list-category', component: ListCategoryComponent },
+  { path: 'edit-category', component: EditCategoryComponent },
+  {path : '', component : LoginComponent}
+];
+
+export const routing = RouterModule.forRoot(routes);
