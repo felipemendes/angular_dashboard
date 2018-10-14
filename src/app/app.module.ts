39cfe9d38bc7227cee1aa16d749fae7f88dd831f
felipemendes <f.ribeiromendes@gmail.com>
2018-10-13 22:10:44 -0300

    Generate login and category components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { EditCategoryComponent } from './edit-category/edit-category.component';
+import { ListCategoryComponent } from './list-category/list-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AddCategoryComponent,
+    EditCategoryComponent,
+    ListCategoryComponent
   ],
   imports: [
     BrowserModule
