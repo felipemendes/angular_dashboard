c1f5a454ecb68ec30fb8aed19a7b48dbbc8b4016
felipemendes <f.ribeiromendes@gmail.com>
2018-10-14 03:31:17 -0300

    Implement create category

--- src/app/add-category/add-category.component.ts
+++ src/app/add-category/add-category.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CategoryService } from "../service/category.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
+
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,private router: Router, private categoryService: CategoryService) { }
+
+  addForm: FormGroup;
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [],
+      uuid: [],
+      status: ['', Validators.required],
+      title: ['', Validators.required],
+      url_image: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    this.categoryService.createCategory(this.addForm.value)
+      .subscribe(
+        () => {
+          this.router.navigate(['list-category']);
+        },
+        err => console.log(err)
+      );
+  }
 }
