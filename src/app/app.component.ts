46c3e8d5cb3435d525712d598cc46f7a9a5dd014
felipemendes <f.ribeiromendes@gmail.com>
2018-10-15 00:10:34 -0300

    Add list layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { TokenStorage } from './core/token.storage';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
